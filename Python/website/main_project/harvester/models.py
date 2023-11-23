04b60954a4bcd0d401ba4f8d949facac88ac16e3
Orlando Nascimento <on_ferreira@id.uff.br>
2023-11-23 13:27:39 -0300

    Transforming into 1 project with multiple apps.

---
+++ Python/website/main_project/harvester/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    coach = models.CharField(max_length=255)
+    foundation_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=255)
+    birth_date = models.DateField()
+    height = models.FloatField()
+    position = models.CharField(max_length=255)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    date = models.DateField()
+    home_team = models.ForeignKey(Team, related_name='home_team', on_delete=models.CASCADE)
+    away_team = models.ForeignKey(Team, related_name='away_team', on_delete=models.CASCADE)
+    home_team_score = models.IntegerField()
+    away_team_score = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.home_team} vs {self.away_team} on {self.date}"
+
+
+class GameStatistics(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    MP = models.IntegerField()
+    FG = models.IntegerField()
+    FGA = models.IntegerField()
+    FG_percentage = models.FloatField()
+    ThreeP = models.IntegerField()
+    ThreePA = models.IntegerField()
+    ThreeP_percentage = models.FloatField()
+    FT = models.IntegerField()
+    FTA = models.IntegerField()
+    FT_percentage = models.FloatField()
+    ORB = models.IntegerField()
+    DRB = models.IntegerField()
+    TRB = models.IntegerField()
+    AST = models.IntegerField()
+    STL = models.IntegerField()
+    BLK = models.IntegerField()
+    TOV = models.IntegerField()
+    PF = models.IntegerField()
+    PTS = models.IntegerField()
+    plus_minus = models.IntegerField()
+
+    def __str__(self):
+        return f"Statistics of {self.player} in the game {self.game}"
